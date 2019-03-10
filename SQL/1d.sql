3a80a7b82c42ab8375bf78ee765fe907de1dc4ae
Whiitetesla <whiite.h.l@gmail.com>
2019-03-10 12:40:54 +0100

    fixed the error of using name insted of id

--- SQL/1d.sql
+++ SQL/1d.sql
@@ -1,44 +1,39 @@
 USE Company;
 GO
 CREATE PROCEDURE usp_DeleteDepartment
-	@Dname varchar(50),
-	@Dnum int OUTPUT
+	@DNumber varchar(50)
 	AS BEGIN
 
 	SET NOCOUNT ON;
 
 	IF(NOT EXISTS (SELECT * FROM Department 
-				WHERE DName = @Dname))
+				WHERE DNumber = @DNumber))
 		BEGIN
 			RAISERROR('Department does not exists', 16, 1);
 		END
-		
-		--Saves the number of the department we want to delete
-		SELECT @Dnum = DNumber FROM Department
-		WHERE DName = @Dname
 
 		--Removes all Works_on that are connectet to the project in that department
 		Delete From Works_on
 		Where Pno in (
 			SELECT PNumber FROM Project
-			WHERE DNum = @Dnum)
+			WHERE DNum = @DNumber)
 
 		--Removes all project that were in that department
 		Delete From Project
-		Where DNum = @Dnum
+		Where DNum = @DNumber
 
 		--Set all emplyee that were in that depatment to have null insted
 		--Feel sad form them :(
 		UPDATE Employee
 		SET Dno = null
-		WHERE Dno = @Dnum
+		WHERE Dno = @DNumber
 
 		--Removes the linked location of the deleted depatment
 		DELETE FROM Dept_Locations
-		WHERE DNUmber = @Dnum
+		WHERE DNUmber = @DNumber
 
 		--Delete the department 
 		--Now its all gone ...
 		DELETE FROM Department
-		WHERE DNumber = @Dnum	
+		WHERE DNumber = @DNumber	
 	END
